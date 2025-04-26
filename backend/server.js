5830b7e339c1346793aec0c63b9db7dd8d11bb3c
Prakash-Punj <prakashpunj29@gmail.com>
2025-04-26 15:58:27 +0530

    Updated server to listen on 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,7 @@ app.post('/api/contact', async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
